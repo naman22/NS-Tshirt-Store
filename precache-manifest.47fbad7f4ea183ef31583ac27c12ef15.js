96f1e302a9b9d7115949497e1af2a7283c143ede
naman22 <naman.shukla_cs18@gla.ac.in>
2021-07-22 20:51:41 +0530

    Updates

---
+++ precache-manifest.47fbad7f4ea183ef31583ac27c12ef15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e638eb167674048bc183b8ee3f29036",
+    "url": "/NS-Tshirt-Store/index.html"
+  },
+  {
+    "revision": "ada2f19014c2fc89eeb6",
+    "url": "/NS-Tshirt-Store/static/css/main.00cf29bb.chunk.css"
+  },
+  {
+    "revision": "e63a9dfa37818bfba14e",
+    "url": "/NS-Tshirt-Store/static/js/2.521ca244.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/NS-Tshirt-Store/static/js/2.521ca244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada2f19014c2fc89eeb6",
+    "url": "/NS-Tshirt-Store/static/js/main.bcfa8e5b.chunk.js"
+  },
+  {
+    "revision": "b175f58146d2a77e764c",
+    "url": "/NS-Tshirt-Store/static/js/runtime-main.1d357fb8.js"
+  }
+]);
